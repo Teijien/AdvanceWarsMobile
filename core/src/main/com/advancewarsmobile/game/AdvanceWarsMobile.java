0571eb8b05bc397163e9c1f19f394b83eb839cd3
Teijien <tjhansen@ucsc.edu>
2022-01-15 22:54:55 -0800

    City tiles added

--- core/src/main/com/advancewarsmobile/game/AdvanceWarsMobile.java
+++ core/src/main/com/advancewarsmobile/game/AdvanceWarsMobile.java
@@ -10,17 +10,19 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 
 public class AdvanceWarsMobile extends ApplicationAdapter {
 	private static final int WORLD_WIDTH = 480;
 	private static final int WORLD_HEIGHT = 640;
-	private static final float TILE_SIZE = 1 / 80f;
+	private static final String MAP_NAME = "map.tmx";
 
 	private SpriteBatch batch;
+	// Map
 	private TiledMap map;
+	private Map game;
+
+	// Camera
 	private OrthogonalTiledMapRenderer renderer;
 	private OrthographicCamera camera;
 	private ExtendViewport viewport;
@@ -28,10 +30,13 @@ public class AdvanceWarsMobile extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		map = new TmxMapLoader().load("map.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map);
+		//batch = new SpriteBatch();
+		// Map init
+		map = new TmxMapLoader().load(MAP_NAME);
+		game = new Map(map);
 
+		// Camera and Renderer init
+		renderer = new OrthogonalTiledMapRenderer(map);	// DO NOT SET "unitscale"! Messes with calcs
 		camera = new OrthographicCamera(WORLD_WIDTH, WORLD_HEIGHT);
 		camera.setToOrtho(false, WORLD_WIDTH, WORLD_HEIGHT);
 		viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
@@ -39,20 +44,21 @@ public class AdvanceWarsMobile extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(Color.BLUE);
+		ScreenUtils.clear(Color.BLUE);	// Screen must be refreshed before redrawing
 
+		// Viewport gets updated instead of camera, as all control of camera passes through viewport
 		viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		renderer.setView(camera);
+		renderer.setView(camera);	// Sets the placement of the map under the camera
 		renderer.render();
 
-		batch.begin();
-		batch.end();
+		//batch.begin();
+		//batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		//batch.dispose();
 		map.dispose();
 	}
 }
--- core/src/main/com/advancewarsmobile/game/Map.java
+++ core/src/main/com/advancewarsmobile/game/Map.java
@@ -1,65 +1,30 @@
 package com.advancewarsmobile.game;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileSet;
 
-/* Definition of Map class
-*
-*  Contains the tilemaps as an array of ints representing
-*  tile types and methods to display them. */
+
+/* Definition of Map class */
 public class Map {
-    public enum Tile {
-        PLAINS
-    }
-    private Texture plainTile;
-    private Texture cityTile;
-    private Tile[][] tilemap;
+    private static final String MAP_TILES = "plainsTile-border";
+    private static final int PLAINS_ID = 0;
+
     private TiledMap map;
+    private TiledMapTileSet tileset;
 
-    private Map() {}
 
-    public Map(Texture plainTile, Texture cityTile, TiledMap map, int w, int h) {
-        this.plainTile = plainTile;
-        this.cityTile = cityTile;
+    public Map(TiledMap map) {
         this.map = map;
-        tilemap = fillWithPlains(w, h);
+        tileset = map.getTileSets().getTileSet(MAP_TILES);
     }
 
-    public Tile[][] getTilemap() {
-        return tilemap;
+    private Map () {}
+
+    private TiledMap getTiledMap() {
+        return map;
     }
 
-    /* Method fillWithPlains(int, int)
-    *  This method is used to fill the map with
-    *  Plains tiles */
-    private Tile[][] fillWithPlains(int w, int h) {
-        Tile[][] t;
-
-        if (w == 0 || h == 0) {
-            System.out.println("Map size cannot be 0");
-            return null;
-        }
-
-        if (w > h) {
-            int temp = w;
-            w = h;
-            h = temp;
-        }
-
-        try {
-            t = new Tile[w][h];
-        } catch(NegativeArraySizeException e) {
-            System.out.println("Array size should not be negative");
-            return null;
-        }
-
-        // Set all tiles in map to Plains
-        for (int i = 0; i < t.length; i++) {
-            for (int j = 0; j < t[0].length; j++) {
-                t[i][j] = Tile.PLAINS;
-            }
-        }
-
-        return t;
+    private TiledMapTileSet getTileSet() {
+        return tileset;
     }
 }
